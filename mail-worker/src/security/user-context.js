8d68614b81e7e8132cf834a4f80af65ffbaa01c5
qwq_ZWyu <txp110905@163.com>
2026-07-03 20:01:39 +0800

    Update user-context.js
    fix bugs

--- mail-worker/src/security/user-context.js
+++ mail-worker/src/security/user-context.js
@@ -1,19 +1,22 @@
+import BizError from '../error/biz-error';
 import JwtUtils from '../utils/jwt-utils';
 import constant from '../const/constant';
 
 const userContext = {
-	getUserId(c) {
-		return c.get('user').userId;
-	},
-
-	getUser(c) {
-		return c.get('user');
-	},
-
-	async getToken(c) {
-		const jwt = c.req.header(constant.TOKEN_HEADER);
-		const result = await JwtUtils.verifyToken(c,jwt);
-		return result?.token;
-	},
+    getUserId(c) {
+        const user = c.get('user');
+        if (!user) {
+            throw new BizError('用户信息异常，请重新登录', 401);
+        }
+        return user.userId;
+    },
+    getUser(c) {
+        return c.get('user');
+    },
+    async getToken(c) {
+        const jwt = c.req.header(constant.TOKEN_HEADER);
+        const result = await JwtUtils.verifyToken(c, jwt);
+        return result?.token;
+    },
 };
 export default userContext;
